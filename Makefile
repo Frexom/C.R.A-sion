a2dffbe875c788c80666bbac1e4e162a31d41366
Frexom <imfrexom@gmail.com>
2023-12-04 13:09:10 +0100

    Added makefile, changed import paths

---
+++ Makefile
@@ -0,0 +1,5 @@
+server:
+	poetry run python -m src.server.main
+
+bot:
+	poetry run python -m src.api.bot
--- src/api/crasion.py
+++ src/api/crasion.py
@@ -1,4 +1,5 @@
-import j2l.pytactx.agent as pytactx
+import src.api.j2l.pytactx.agent as pytactx
+
 
 class Survivor:
     def __init__(
@@ -17,25 +18,33 @@ class Survivor:
         welcomePrint: bool = True,
         sourcesdir: str or None = None,
     ):
-        self.__agent = pytactx.Agent(playerId, arena, username, password, server,
-            port, imgOutputPath, autoconnect, waitArenaConnection, verbosity,
-            robotId, welcomePrint, sourcesdir)
-        
+        self.__agent = pytactx.Agent(
+            playerId,
+            arena,
+            username,
+            password,
+            server,
+            port,
+            imgOutputPath,
+            autoconnect,
+            waitArenaConnection,
+            verbosity,
+            robotId,
+            welcomePrint,
+            sourcesdir,
+        )
+
         self.weapon = ""
         self.type = "survivor"
-    
 
     def move(self, dx: int, dy: int) -> None:
         self.__agent.move(dx, dy)
-    
 
     def moveTowards(self, x: int, y: int) -> None:
         self.__agent.moveTowards(x, y)
-    
 
     def lookAt(self, dir: int) -> None:
         self.__agent.lookAt(dir)
-    
 
     def fire(self, enable: bool = True) -> None:
         self.__agent.fire(enable)
